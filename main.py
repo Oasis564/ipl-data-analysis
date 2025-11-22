44901a76e1f9194247e392d2eb4248a743095c72
osaeid rony <osaeidfire@gmail.com>
2025-11-22 16:22:23 +0000

    We made different ways to make lists and how we can use them

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
+# read all the content from IPL_2018.csv
+
+# with open("IPL_2018.csv", 'r') as f:
+#     content = f.read()
+#     print(content)
+    
+# read the entire file and print each row
+
+# with open("IPL_2018.csv", 'r') as f:
+#     content = f.readlines()
+#     for i in range(0, len(content)):
+#         # By putting square brackets then i inside of the print next to content we are actually starting from the first line till line 144(all the lines in the file).
+#         print(content[i])
+        
+# read the entire file and print each row using the CSV module
+
+# import csv
+
+# with open("IPL_2018.csv", 'r') as f:
+#     content = csv.reader(f)
+#     for row in content:
+#         print(row)
+
+# read the entire file and print each row using the CSV module and ',' as the delimeter
+
+# import csv
+
+# with open("IPL_2018.csv", 'r') as f:
+#     content = csv.reader(f,delimiter=",")
+#     for row in content:
+#         print(row)
+
+# read the entire file and print each row using the CSV module and convert th content into a list before printing
+  
 import csv
 
-with open("IPL_2018.csv"):
-    data = csv.reading()
-    for i 
+with open("IPL_2018.csv", 'r') as f:
+    content = csv.reader(f)
+    content_list = list(content)
+    for i in range(0, len(content_list)):
+        print(content_list[i])
